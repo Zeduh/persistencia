e6a0ab02c3b36561a2249edee2fabc081d2b10e7
Zeduh <jeduardocury@gmail.com>
2023-08-29 16:56:38 -0300

    ALterado para JpaRepository e deletado ProdutoDao.java

--- src/main/java/br/com/tex/persistencia/jpa/ProdutoRepository.java
+++ src/main/java/br/com/tex/persistencia/jpa/ProdutoRepository.java
@@ -1,17 +1,6 @@
 package br.com.tex.persistencia.jpa;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface ProdutoRepository extends Repository<Produto, Integer> {
-    void save(Produto produto);
-
-    List<Produto> findAll();
-
-    Optional<Produto> findById(Integer id);
-
-    void deleteById(Integer id);
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 }
